65d14d3043f69c34b7b6d3fd781b048ae4986d01
sikcrods <74572426+sikcrods@users.noreply.github.com>
2020-11-23 16:01:18 -0500

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Air-sea Package for Python
+    Version 1.0 Oct. 8, 2020
+    Author:  Brice Loose, Graduate School of Oceanography, URI.
+    Email: bloose@uri.edu
+    # 
+    # DISCLAIMER:
+    #    This software is provided "as is" without warranty of any kind. 
+    #=========================================================================      
+"""
+
+#import numpy as np
+from .air_sea import *
+
+
+__all__ = ['sat_vp']
